3f7545d1b28fb2a5e55f7ded6a90679f7f9542bd
kanades <3080569+qn1213@users.noreply.github.com>
2020-04-12 15:54:44 +0900

    Update

--- Day1/Day1/Main.cpp
+++ Day1/Day1/Main.cpp
@@ -1,7 +1,21 @@
-#include <iostream>
+#include "Player.h"
 
 int main()
 {
+	Player *player = new Player();
 
+	player->Move();
+	player->Move();
+	player->Attack();
+	player->Move();
+	player->Attack();
+	player->Eat();
+	player->Move();
+	player->Attack();
+	player->Attack();
+	player->Attack();
+	player->Idle();
+
+	delete player;
 	return 0;
 }
